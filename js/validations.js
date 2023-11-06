8b21cdd752bddf6ad5640e3fa49dda0d443e2ddf
LeudiX <leudix.rafael@gmail.com>
2023-11-06 00:44:38 -0500

    Added new validations on form-input-check class

--- js/validations.js
+++ js/validations.js
@@ -1,7 +1,6 @@
 /**
-* ! Handling validations with Js in the signInModal Form
+* ! Handling validations with Js in the signInModal Form (1st Modal)
 */
-
 // Targeting all my classes and id from the HTML template
 let id = (id) => document.getElementById(id);
 
@@ -49,9 +48,8 @@ let formValid = (id, serial, message) => {
 }
 //==============================================================================================
 /**
-* ! Handling validations with Js in the signUpModal Form
+* ! Handling validations with Js in the signUpModal Form (2nd Modal)
 */
-
 //Storing Second Modal form classes and id's inside these variables
 let username = id("username"),
     email2 = id("email2"),
@@ -96,7 +94,7 @@ let formValid2 = (id, serial, message) => {
 }
 //==============================================================================================
 /**
-* ! Handling validations with Js in the Missed Pet Data Entry Form Modal
+* ! Handling validations with Js in the Missed Pet Data Entry Form Modal (3rd Modal)
 */
 //Storing Third Modal form classes and id's inside these variables
 let petName = id("petName"),
@@ -108,6 +106,8 @@ let petName = id("petName"),
     petAge = id("petAge"),
     petSize = id("petSize"),
     city = id("city"),
+    petWasFound = id("petWasFound"),
+    checkboxes = id("checkBoxes"),
     comment = id("comment"),
     contactName = id("contactName"),
     contactPhone = id("contactPhone"),
@@ -118,7 +118,7 @@ let petName = id("petName"),
     successIcon3 = classes("success-icon3"),
     failureIcon3 = classes("failure-icon3");
 
-//Specific errors messages for each input field
+//Specific errors messages for each input field in the 3rd form
 const petNameM = ["Pet name is required", "Pet name must be between 2 and 8 characters"];
 const petPictureM = "Pet picture is required and most be in the specified format: .jpg, .jpeg, .png, .gif";
 const petTypeM = ["Pet type is required", "Pet type must be between 3 and 8 characters"];
@@ -127,9 +127,43 @@ const commentM = ["Comments field is required", "Comments must be between 20 and
 const contactNM = ["Contact name field is required", "Contact name must be between 3 and 8 characters"];
 const contactPM = ["Contact phone field is required", "Valid phone number must be between 7 and 15 digits", "Invalid phone number"];
 const contactEM = ["Contact email field is required", "Invalid email"];
+const petWasDM = ["You must select at least one of the elements in the checkbock list"];
 
 
-// Targeting Third Modal form and add the submit event listener
+//Show and hide a list of checkboxes when clicking on the select input type with id = 'petWasFound' 
+let show = true;
+
+function showCheckboxes() {
+
+    if (show) {
+        checkboxes.style.display = "block";
+        show = false;
+    } else {
+        checkboxes.style.display = "none";
+        show = true;
+    }
+}
+
+/**
+ * !Declarations for checkbox list validations
+ */
+
+//Obtain all form-check-input classes for store his values and future processing
+let checkblist = (checkblist) => document.getElementsByClassName(checkblist);
+
+//Store all form-check-input classes inside a HTML (Live) Collection. It's not a good practice iterate over a Live Collection
+let formcheckinput = checkblist("form-check-input");
+
+//Array for future  checkbox items collections transfer 
+let checkbArr = [];
+
+//Add the HTML (Live) Collection stored in [formcheckinput] to an array
+checkbArr = [...formcheckinput]; //Using ...(spread operator) to transfer the items into the array
+
+
+/**
+ * !Targeting Third Modal form and add the submit event listener
+ */
 form3.addEventListener("submit", (e) => {
 
     //Prevent the form from submmiting event
@@ -150,6 +184,9 @@ form3.addEventListener("submit", (e) => {
 
     //Validating the correct format of the email input field and that it is not empty
     isEmailValid(contactEmail, 14, contactEM);
+
+    //Validating the multiple checkbox list inside the form
+    checkListValid(petWasFound, checkbArr, 10, petWasDM);
 });
 
 //Not Blank and desired input fields length validations
@@ -217,6 +254,70 @@ const isPhoneValid = (id, min, max, serial, messages) => {
     }
     return valid;
 }
+
+
+//form-check-input class items Validation
+const checkListValid = (id, checklist, serial, message) => {
+    let valid = false;
+    let noncheck = 0; //count nonchecked elements on the checkbox list
+    let checked = 0;  //count checked elements on the checkbox list 
+    let textnode = [];
+    const node = document.createElement("option");
+
+    for (let i = 0; i < checklist.length; i++) {
+
+        if (!checklist[i].checked) {
+            noncheck++;
+        } else
+            textnode.push(document.createTextNode(checklist[i].value));
+        checked++;
+    }
+
+    if (noncheck == checklist.length) {
+        showErrorM(id, serial, message[0]); //showErrorM
+        id.value = id.firstElementChild.value; //change to default disabled value of select
+        //console.info("There are " + noncheck + " checkboxs unchecked "); //Testing Outputs
+    } else {
+        showSuccessM(id, serial);
+
+        textnode.forEach((e) => {
+            node.appendChild(e);
+            id.value = node.value;
+        });
+
+        id.appendChild(node);
+
+        //console.info("At least " + checked + " checkboxs are checked"); //Testing Outputs
+        valid = true;
+    }
+
+    /*Testing Outputs
+    console.info(formcheckinput.item(0).id);
+    console.info(formcheckinput.item(0).value);
+
+    console.info(checklist[i].id);
+    console.info(checklist[i].value);
+    console.info(checklist[i].checked);     
+    
+    checklist[i].addEventListener("change", () => {
+        if (!checklist[i].checked) {
+            showErrorM(id, serial, message[0]); //showErrorM
+            console.info("Checkbox: " + checklist[i].id + " isn't checked. ");
+        } else {
+            showSuccessM(id, serial);  //showSuccessM
+            console.info("Checkbox: " + checklist[i].id + " is checked. Adding to checklist!!");
+            chequed.push(checklist[i].value);
+            check++;
+            console.info("At least " + check + " elements are chequed");
+            valid = true;
+        }
+        for (let i = 0; i < chequed.length; i++) { //Loop
+            console.info(chequed[i]);
+        }
+    });*/
+    return valid;
+}
+//==================================================================================================
 
 //3rd Modal not empty field validations
 let notBlankValid = (id) => {
@@ -294,55 +395,4 @@ const showSuccessM = (id, serial) => {
     //icons
     failureIcon3[serial].style.opacity = "0";
     successIcon3[serial].style.opacity = "1"; //showing green success icon
-}
-//==================================================================================================
-let show = true;
-
-//Obtain all form-check-input classes for store his values and future processing
-let checkblist = (checkblist) => document.getElementsByClassName(checkblist);
-
-//Store all form-check-input classes inside a HTML (Live) Collection. It's not a good practice iterate over a Live Collection
-let formcheckinput = checkblist("form-check-input");
-
-//Array for future  checkbox items collections transfer 
-let checkbArr = [];
-
-//Add the HTML (Live) Collection stored in [formcheckinput] to an array
-checkbArr = [...formcheckinput]; //Using ...(spread operator) to transfer the items into the array
-/*
-console.info(formcheckinput.item(0).id);
-console.info(formcheckinput.item(0).value);
-*/
-
-//Get all form-check-input classes from the array
-const showCheckListVal = (checklist) => {
-    for (let i = 0; i < checklist.length; i++) {
-        /*
-        console.info(checklist[i].id);
-        console.info(checklist[i].value);
-        console.info(checklist[i].checked);
-        */
-        checklist[i].addEventListener("change", () => {
-            if (!checklist[i].checked) {
-                console.info( "Check box: "+ checklist[i].id +" isn't checked. ");
-            } else {
-                console.info( "Check box: "+ checklist[i].id +" is checked. ");
-            }
-        });
-    }
-}
-
-showCheckListVal(checkbArr);
-
-//Show and hide a list of checkboxes when clicking on a select input type with id = 'petWasFound' 
-function showCheckboxes() {
-    let checkboxes = document.getElementById("checkBoxes");
-
-    if (show) {
-        checkboxes.style.display = "block";
-        show = false;
-    } else {
-        checkboxes.style.display = "none";
-        show = true;
-    }
 }
